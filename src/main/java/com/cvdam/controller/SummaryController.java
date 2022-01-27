6af1e0c7705bb7afcd8d33923168b1daebae33ce
cvdam <cassiovdonato@gmail.com>
2022-01-27 10:27:08 -0300

    Return all summary categories, even if no expenses with zero

--- src/main/java/com/cvdam/controller/SummaryController.java
+++ src/main/java/com/cvdam/controller/SummaryController.java
@@ -2,6 +2,7 @@ package com.cvdam.controller;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +11,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cvdam.model.Category;
 import com.cvdam.reports.CategorySummary;
 import com.cvdam.reports.Summary;
+import com.cvdam.repository.CategoryRepository;
 import com.cvdam.repository.ExpenseRepository;
 import com.cvdam.repository.IncomeRepository;
 
@@ -24,6 +27,9 @@ public class SummaryController {
 	
 	@Autowired
 	private IncomeRepository incomeRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 		
 	@GetMapping("/{year}/{month}")
 	public Summary generateSummary(@PathVariable Integer year, @PathVariable Integer month){
@@ -36,7 +42,25 @@ public class SummaryController {
 		BigDecimal totalExpenses = expenseRepository.findTotalExpensesByDate(startDate, endDate);
 		List<CategorySummary> expensesByCategory = expenseRepository.findTotalExpensesByCategory(startDate, endDate);
 
-		Summary summary = new Summary(totalIncomes, totalExpenses, expensesByCategory);
+		List<Category> categories = categoryRepository.findAll();
+		List<CategorySummary> summaryCategoriesResults = new ArrayList<>();
+		
+		for(int i =0 ; i< categories.size(); i++) {
+			for(int j = 0; j <expensesByCategory.size(); j++) {
+				if (categories.get(i).getName().equals(expensesByCategory.get(j).getName())) {
+					summaryCategoriesResults.add(expensesByCategory.get(j));
+					categories.remove(i);
+				}
+			}
+		}
+		
+		BigDecimal bd = new BigDecimal(0);
+		for(int c =0 ; c< categories.size(); c++) {
+			CategorySummary cs = new CategorySummary(categories.get(c).getName(), bd);
+			summaryCategoriesResults.add(cs);
+		}
+		
+		Summary summary = new Summary(totalIncomes, totalExpenses, summaryCategoriesResults);
 		return summary;
 
 	}
